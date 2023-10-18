6371befc9968590ee214d5908bceb2b4164a8d17
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-10-18 13:44:34 +1100

    removed commented code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    Board board = new Board(10, 10);
-    board.initializeCells(10);
-    board.printCells();
+    // Board board = new Board(10, 10);
+    // board.initializeCells(10);
+    // board.printCells();
 
-    int randomRow = (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
-    int randomCol = (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
-    board.makeMove(randomRow, randomCol);
-    System.out.println("");
-    board.printCells();
+    // int randomRow = (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
+    // int randomCol = (int) Math.floor(Math.random() * (9 - 0 + 1) + 0);
+    // board.makeMove(randomRow, randomCol);
+    // System.out.println("");
+    // board.printCells();
 
     Game game = new Game(10, 10, 5);
     // game.playerMove();
